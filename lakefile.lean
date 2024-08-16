d6e65f6a0edb193eb4ec2dee04fe0e01667cd88b
SDZZGNDRC <sdzzgndrc@gmail.com>
2024-08-16 21:40:02 +0800

    first commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «src» where
+  -- add package configuration options here
+
+-- lean_lib «Src» where
+--   -- add library configuration options here
+
+@[default_target]
+lean_lib «src» where
